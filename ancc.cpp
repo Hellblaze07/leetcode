513cb3bd10c9645164a5852f8c9c137fbb1d2b39
Hellblaze07 <jayeshkejriwal237@gmail.com>
2026-06-22 15:47:06 +0530

    first push

---
+++ ancc.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+       cout << "HELLo " << endl;
+    }
+    return 0;
+}
